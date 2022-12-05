25892b3006409aa6f2df3d285e14cc8279ed0c45
Natalia Romaszko <natalia.romaszko@gmail.com>
2022-12-05 11:12:49 +0100

    added for const class button and emoji

--- js/script.js
+++ js/script.js
@@ -7,8 +7,14 @@
 
         for (const task of tasks) {
             htmlString += `
-            <li>
-            ${task.content}
+            <li class="list js-tasks">
+            <button class="list__textButton list__textButton--done js-buttonDone">
+            ${task.done ? "✅" : ""} 
+            </button>
+            <span class="list__content${task.done ? "list__content--done" : ""}">
+            ${task.content}</span>
+            <button class="list__textButton list__textButton--remove js-textRemove">
+            🗑️</button>
             </li>
             `;
         }
